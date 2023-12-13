2d486c41c025a91e9534a05ee9417ac26cab68c5
Parthiv0311 <122387846+Parthiv0311@users.noreply.github.com>
2023-12-12 21:02:46 -0500

    Question 3 Complete

--- Test 2 Start/src/main/java/com/georgiancollege/test2/ApiResponse.java
+++ Test 2 Start/src/main/java/com/georgiancollege/test2/ApiResponse.java
@@ -1,9 +1,35 @@
 package com.georgiancollege.test2;
 
+import com.google.gson.Gson;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
 public class ApiResponse {
     private User[] users;
 
     public User[] getUsers() {
+
         return users;
+    }
+
+    // Method to fetch and parse JSON from a remote URL
+    public static User[] fetchJsonFromUrl(String urlString) {
+        Gson gson = new Gson();
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            try (InputStreamReader reader = new InputStreamReader(con.getInputStream())) {
+                ApiResponse response = gson.fromJson(reader, ApiResponse.class);
+                return response.getUsers();
+            } finally {
+                con.disconnect();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
