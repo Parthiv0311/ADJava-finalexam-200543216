f879f231f437ca8c8ffec0ce741c798516b62d93
Parthiv0311 <122387846+Parthiv0311@users.noreply.github.com>
2023-12-12 20:38:08 -0500

    Question 1 Complete

--- Test 2 Start/src/main/java/com/georgiancollege/test2/User.java
+++ Test 2 Start/src/main/java/com/georgiancollege/test2/User.java
@@ -1,5 +1,61 @@
+
 package com.georgiancollege.test2;
 
+import java.util.Date;
+import com.google.gson.annotations.SerializedName;
+
 public class User {
-    //testing
+
+    // Instance variables as per the JSON structure
+    private int id;
+    @SerializedName("FirstName") private String firstName;
+    @SerializedName("LastName") private String lastName;
+    private int age;
+    private String email;
+    private String phone;
+    @SerializedName("birthDate") private Date birthDate; // Assuming date format is compatible
+    @SerializedName("uNiVersity") private String university;
+    private String image;
+    private Address address; // Using Address class for the address field
+
+    // Getter methods
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
 }
