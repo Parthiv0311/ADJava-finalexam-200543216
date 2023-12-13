f41c0cd002a2e7e9985a9351e721b8dfcf497079
Parthiv0311 <122387846+Parthiv0311@users.noreply.github.com>
2023-12-12 20:46:50 -0500

    Question 2 Complete

--- Test 2 Start/src/main/java/com/georgiancollege/test2/Address.java
+++ Test 2 Start/src/main/java/com/georgiancollege/test2/Address.java
@@ -1,4 +1,28 @@
+
 package com.georgiancollege.test2;
 
 public class Address {
+
+    // Instance variables as per the JSON structure
+    private String address;
+    private String city;
+    @SerializedName("postalCode") private String postalCode;
+    private String state;
+
+    // Getter methods
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public String getState() {
+        return state;
+    }
 }
